8b72466e6a1cfedd9cadacde7c2834d88656c8b0
Corey Levine <coreylevine2000@gmail.com>
2022-08-03 21:32:34 -0400

    setting up skeleton for client app js

--- client/src/App.js
+++ client/src/App.js
@@ -3,15 +3,57 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import {ApolloProvider} from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
 
-import HomeStrap from './pages/Home';
+import Home from './pages/Home';
 import Menu from './pages/Menu.js';
-import MyOrder from './pages/MyOrder';
+import Order from './pages/Order';
 import History from './pages/History';
-import SignupStrap from './pages/Signup';
-import LoginStrap from './pages/Login';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import NoMatch from "./pages/NoMatch";
 import Success from "./pages/Success";
 
-import HeaderStrap from './components/Header'
-import FooterStrap from './components/Footer'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import NavStrap from './components/Nav';
+
+const client = new ApolloClient({
+    request: (operation) => {
+      const token = localStorage.getItem('id_token')
+      operation.setContext({
+        headers: {
+          authorization: token ? `Bearer ${token}`: ''
+        }
+      })
+    },
+    uri: 'http://localhost:3001/graphql',
+  })
+  
+  function App() {
+    return (
+      <ApolloProvider client={client}>
+        <Router>
+          <div>
+            <StoreProvider>
+                <Header />
+                <NavStrap />
+                <Switch>
+                  <Route exact path="/" element={<Home />} />
+                  <Route exact path="/menu" element={<Menu />} />
+                  <Route exact path="/login" element={<Login />} />
+                  <Route exact path="/signup" element={<Signup />} />
+                  <Route exact path="/cart" element={<Order />} />
+                  <Route exact path="/history" element={<History />} />
+                  <Route exact path="/success" element={<Success />} />
+                  <Route element={<Home />} />
+                  <Route exact path="*" element={<NoMatch />} />
+                </Switch>
+                <Footer />
+            </StoreProvider>
+          </div>
+        </Router>
+      </ApolloProvider>
+      
+    );
+  }
+  
+  export default App;
