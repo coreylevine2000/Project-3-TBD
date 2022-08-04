883d92ec07a0c8e79d3a30446a9ff69fa5fe92de
Laura Antunez <lauraantunez024@gmail.com>
2022-08-04 19:10:01 -0400

    completed Drink model

---
+++ server/models/Drink.js
@@ -0,0 +1,27 @@
+const Schema = require('mongoose');
+
+optionsSchema = require('./Options')
+
+const drinkSchema = newSchema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    image: {
+        type: String
+    },
+    category: [optionsSchema]
+
+})
+
+const Drink = model("Drink", drinkSchema)
