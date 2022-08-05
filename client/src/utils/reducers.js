1a1b47e2b1ca5774841549ac00d317bcceda14fe
Corey Levine <coreylevine2000@gmail.com>
2022-08-04 20:31:46 -0400

    utils updates

---
+++ client/src/utils/reducers.js
@@ -0,0 +1,10 @@
+import { useReducer } from "react";
+import {
+    UPDATE_DRINKS,
+    ADD_TO_CART,
+    REMOVE_FROM_CART,
+    ADD_MULTIPLE_TO_CART,
+    UPDATE_CATEGORIES,
+    UPDATE_CURRENT_CATEGORY,
+    CLEAR_CART
+  } from "./actions";
