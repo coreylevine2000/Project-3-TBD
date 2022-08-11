ecf2587bbb7d4e9933ecf0763a18980e5d3f67a4
Laura Antunez <lauraantunez024@gmail.com>
2022-08-11 18:29:05 -0400

    changed card styling

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,31 +1,131 @@
 const db = require('../config/connection');
-const { User, Thought } = require('../models');
-const userSeeds = require('./userSeeds.json');
-const thoughtSeeds = require('./thoughtSeeds.json');
+const { User, Drink, Options } = require('../models');
+// import Margarita from '../../client/src/images/margarita.jpg'
+// import OldFashion from '../../client/src/images/oldfashion.jpg'
+// import Martini from '../../client/src/images/martini.jpg'
+// import LongIslandTea from '../../client/src/images/longislandicetea.jpg'
+// import Gingerale from '../../client/src/images/gingerale.jpg'
+// import VMojito from '../../client/src/images/viginmojito.jpg'
+// import Suicide from '../../client/src/images/suicidenoalcohol.jpg'
+
 
 db.once('open', async () => {
-  try {
-    await Thought.deleteMany({});
-    await User.deleteMany({});
+    await Options.deleteMany();
 
-    await User.create(userSeeds);
+    const options = await Option.insertMany([
+        { name: 'Beer' },
+        { name: 'Liquor' },
+        { name: 'Non-Alcoholic' },
+        // { name: 'Tasty Treats' },
+    ]);
 
-    for (let i = 0; i < thoughtSeeds.length; i++) {
-      const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-      const user = await User.findOneAndUpdate(
-        { username: thoughtAuthor },
+    console.log('categories seeded');
+
+    await Drink.deleteMany();
+
+    const drinks = await Drink.insertMany([
+      
         {
-          $addToSet: {
-            thoughts: _id,
-          },
-        }
-      );
-    }
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
+            name: 'Margarita',
+            description: 'A classic lime cocktail with lime juice, tequila and agave syrup.',
+            image: "../../client/src/images/margarita.jpg",
+            option: options[1]._id,
+            price: 7.50,
+            quantity: 1
+        },
+        {
+            name: 'Old Fashion',
+            description: 'Oldy but goldy drink Traditionally made with bourbon or rye whiskey, lightly sweetened with sugar and aromatised with bitters.',
+            image: '../../client/src/images/oldfashion.jpg',
+            option: options[1]._id,
+            price: 9.50,
+            quantity: 1
+        },
+        {
+            name: 'Martini',
+            description: 'a cocktail made with gin and vermouth, and garnished with an olive or a lemon twist.',
+            image: '../../client/src/images/martini.jpg',
+            option: options[1]._id,
+            price: 8.75,
+            quantity: 1
+        },
+        {
+            name: 'Long Island Ice Tea',
+            description: 'Cocktail made with vodka, tequila, light rum, triple sec, gin, and a splash of cola, which gives the drink the same hue as iced tea.',
+            // image: LongIslandTea,
+            option: options[1]._id,
+            price: 9.00,
+            quantity: 1
+        },
+        {
+            name: 'Suicide',
+            description: 'A cup filled to the brim with every single soft drink available at a restaurants soda machine',
+            // image: Suicide,
+            option: options[2]._id,
+            price: 2.25,
+            quantity: 1
+        },
+        {
+            name: 'Ginger ale',
+            description: 'A carbonated beverage made with ginger root and a sweetener.',
+            // image: Gingerale,
+            option: options[2]._id,
+            price: 3.50,
+            quantity: 1
+        },
+        {
+            name: 'virgin Mojito',
+            description: 'A refreshing drink blend of mint and lemon flavours with no rum',
+            // image: VMojito,
+            option: options[0]._id,
+            price: 5.75,
+            quantity: 1
+        },
+        // {
+        //     name: 'Lavender Lemonade',
+        //     description: 'A lavender infused lemonade',
+        //     image: 'client/public/Images/lavenderlemonade.jpg',
+        //     // category: categories[1]._id,
+        //     price: 4.25,
+        //     quantity: 1
+        // },
+        // {
+        //     name: 'zombie',
+        //     description: 'A famous tropical rum cocktail. It is a tasty blend of rum and fruit juices.',
+        //     image: 'client/public/Images/zombiecocktail.jpg',
+        //     // category: categories[0]._id,
+        //     price: 9.25,
+        //     quantity: 1
+        // },
+        
+      
+      
+    ]);
 
-  console.log('all done!');
-  process.exit(0);
+    console.log('drinks seeded');
+
+    await User.deleteMany();
+
+    await User.create({
+        firstName: 'Pamela',
+        lastName: 'Washington',
+        email: 'pamela@testmail.com',
+        password: 'password12345',
+        orders: [
+            {
+                drinks: [drinks[0]._id, drinks[0]._id, drinks[1]._id]
+            }
+        ]
+    });
+
+    await User.create({
+        firstName: 'Elijah',
+        lastName: 'Holt',
+        email: 'eholt@testmail.com',
+        password: 'password12345'
+    });
+
+    console.log('users seeded');
+
+    process.exit();
 });
